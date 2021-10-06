254060f08da86348ef840956a4961544c5a413bd
Dev Pedro <pedrova69@gmail.com>
2021-10-06 15:07:10 -0300

    add constants

---
+++ src/constants/theme.js
@@ -0,0 +1,40 @@
+const sizes = {
+  verticalSpacing: 20,
+  inputIconSize: 20,
+  inputHeight: 60,
+  radius: {
+    s: 6,
+    m: 12,
+    l: 36,
+  },
+  fontSize: {
+    head1: 32,
+    head2: 28,
+    head3: 22,
+    head4: 18,
+  },
+};
+
+const baseColor = {
+  white: '#ffffff',
+  black: '#262626',
+  lightGray: '#E0E0E0',
+  charchoal: '#383838',
+  bitOrange: '#ff6a0d',
+  bitOrangeAlt: '#de5e0f',
+  bitBlue: '#3378ff',
+};
+
+const colors = {
+  confirmation: '#0f0',
+  error: '#f00',
+  primary: baseColor.bitOrange,
+  primary2: baseColor.bitOrangeAlt,
+  secondary: baseColor.bitBlue,
+  light: baseColor.white,
+  dark: baseColor.black,
+  gray: baseColor.charchoal,
+  lightGray: baseColor.lightGray,
+};
+
+export {colors, sizes, baseColor};
